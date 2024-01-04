9773477ff21f7daff59ebc0d4234e183ef75f808
Niels Lazaroms <n.lazaroms@student.fontys.nl>
2024-01-04 17:46:26 +0100

    Add new components and update route configuration
    Introduced several new components including HomePage, ReservationPage, VisitsPage, and CompetitionPage. These new components were subsequently added to the application's route configuration. Additionally, the input form fields were updated to remove the 'required' attribute to better match needed functionality.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SideBarComponent} from "../side-bar/side-bar.component";
 import {AccountIconComponent} from "../account-icon/account-icon.component";
+import {GraphQLCommunication} from "../services/graphql-communication.service";
+import {NgForOf} from "@angular/common";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {TokenInterceptor} from "../helpers/token.interceptor";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SideBarComponent, AccountIconComponent],
+  imports: [SideBarComponent, AccountIconComponent, NgForOf],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+    // ... other providers
+  ],
 })
 export class HomePageComponent {
+
+  associations: any[] = [];
+
+  constructor(graphQLCommunication: GraphQLCommunication) {
+    graphQLCommunication.getMyAssociations().subscribe({
+      next: (assocations) => {
+        this.associations = assocations;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+
+
+  }
+
 
 }
