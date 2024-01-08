a4d586b8f4da267262a2e06d31218c5b13a36546
Niels Lazaroms <n.lazaroms@student.fontys.nl>
2024-01-08 15:17:30 +0100

    Add new components and update route configuration
    Introduced several new components including HomePage, ReservationPage, VisitsPage, and CompetitionPage. These new components were subsequently added to the application's route configuration. Additionally, the input form fields were updated to remove the 'required' attribute to better match needed functionality.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,33 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#38bdf8",
+          "secondary": "#0369a1",
+          "accent": "#0e7490",
+          "neutral": "#06b6d4",
+          "base-100": "#e5e7eb",
+          "info": "#1e40af",
+          "success": "#22c55e",
+          "warning": "#f97316",
+          "error": "#991b1b",
+        },
+        dark: {
+          "primary": "#38bdf8",
+          "secondary": "#0369a1",
+          "accent": "#0e7490",
+          "neutral": "#06b6d4",
+          "base-100": "#1f2937",
+          "info": "#1e40af",
+          "success": "#22c55e",
+          "warning": "#f97316",
+          "error": "#991b1b",
+        }
+      },
+    ],
+  },
 }
