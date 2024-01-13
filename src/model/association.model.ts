6a96fb2d9eacefaf900bf2ca534ca7d47681a2de
Merijn <mfg.gommeren@student.avans.nl>
2024-01-13 20:36:50 +0100

    Remove redundant comments in association.model.ts
    The unnecessary comments that explain the TypeScript conversions of GraphQL properties in the 'Association' interface have been removed from the association.model.ts file. This makes the code cleaner and more readable by removing clutter and redundant information.

--- src/model/association.model.ts
+++ src/model/association.model.ts
@@ -2,11 +2,11 @@ import {Image} from "./image.model";
 import {UserAssociation} from "./user-association.model";
 
 export interface Association {
-  id: string; // ID! in GraphQL wordt een string in TypeScript
-  name: string; // String! in GraphQL wordt een string in TypeScript
-  image?: Image; // Image in GraphQL is optioneel, dus het wordt ook optioneel in TypeScript
-  welcomeMessage?: string; // String in GraphQL is optioneel, dus het wordt ook optioneel in TypeScript
-  contactEmail?: string; // String in GraphQL is optioneel, dus het wordt ook optioneel in TypeScript
-  active: boolean; // Boolean! in GraphQL wordt een boolean in TypeScript
-  users?: UserAssociation[]; // [UserAssociation] in GraphQL is een array van UserAssociation in TypeScript
+  id: string;
+  name: string;
+  image?: Image;
+  welcomeMessage?: string;
+  contactEmail?: string;
+  active: boolean;
+  users?: UserAssociation[];
 }
