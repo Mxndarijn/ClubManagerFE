3554d13f1e74d2249f12dfe26e7a4f0c5939dfaf
Mxndarijn <40671102+Mxndarijn@users.noreply.github.com>
2024-04-29 15:03:44 +0200

    Update apiUrl in environment config
    Switched the active apiUrl in the environment configuration from an IP address to an azurecontainerapps.io address. The previous IP address apiUrl has been commented out. This change aligns the application with the Azure container hosting environment.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://clubmanager.nicebeach-506c4c3b.westeurope.azurecontainerapps.io',
-    apiUrl: 'http://82.217.140.246:8080',
+    apiUrl: 'https://clubmanager.nicebeach-506c4c3b.westeurope.azurecontainerapps.io',
+    // apiUrl: 'http://82.217.140.246:8080',
     companyName: 'LedenCentraal'
   };
