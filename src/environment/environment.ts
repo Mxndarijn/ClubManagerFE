98458ebf1c16197fa824ab2151f0c569a0bf6642
Mxndarijn <merijn.gommeren@hotmail.com>
2024-10-24 17:23:27 +0200

    Update apiUrl in environment.ts
    Changed the apiUrl for the environment configuration from the previous Azure container apps endpoint to the new Ledencentraal domain. This ensures all API requests are directed to the updated backend service.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://clubmanager-api.jollysky-531bf3e0.westeurope.azurecontainerapps.io',
+    apiUrl: 'https://www.api.ledencentraal.nl',
     // apiUrl: 'http://localhost:8080',
     companyName: 'LedenCentraal'
   };
