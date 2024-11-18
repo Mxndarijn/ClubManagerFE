1e3ea51a4d08c206406ce40f99036a4c44ff9bec
Mxndarijn <merijn.gommeren@hotmail.com>
2024-11-18 20:59:45 +0100

    Configure production API URL
    Uncommented the production API URL and commented out the local development URL. This change ensures that the application points to the correct production endpoint.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://clubmanager-api.jollysky-531bf3e0.westeurope.azurecontainerapps.io',
+    apiUrl: 'https://clubmanager-api.jollysky-531bf3e0.westeurope.azurecontainerapps.io',
     // apiUrl: 'https://www.api.ledencentraal.nl',
-    apiUrl: 'http://localhost:8080',
+    // apiUrl: 'http://localhost:8080',
     companyName: 'LedenCentraal'
   };
